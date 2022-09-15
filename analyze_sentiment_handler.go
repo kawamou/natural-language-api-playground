36098d60c3e3a3af46f7edada87ef92de1f9714b
kawamou <kwmkzsd@gmail.com>
2022-09-15 14:19:26 +0900

    AnalyzeSentimentHandlerを追加

---
+++ analyze_sentiment_handler.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AnalyzeSentimentHandler ...
+type AnalyzeSentimentHandler struct {
+	c *Client
+}
+
+// AnalyzeSentimentHandlerResponse ...
+type AnalyzeSentimentHandlerResponse struct {
+	PositiveNegative string `json:"positive_nagative"`
+}
+
+// NewAnalyzeSentimentHandler ...
+func NeAnalyzeSentimentHandler(c *Client) *AnalyzeSentimentHandler {
+	return &AnalyzeSentimentHandler{
+		c: c,
+	}
+}
+
+// Handle ...
+func (a *AnalyzeSentimentHandler) Handle(c *gin.Context) {
+	ctx := c.Request.Context()
+	text := c.Param("text")
+	ret, err := a.c.AnalyzeSentiment(ctx, text)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	response := &AnalyzeSentimentHandlerResponse{
+		PositiveNegative: ScoreToPositiveNegative(ret.Sentiment.Score),
+	}
+	c.JSON(http.StatusOK, response)
+}
+
+func ScoreToPositiveNegative(score float32) string {
+	if score >= 0 {
+		return "positive"
+	} else {
+		return "negative"
+	}
+}

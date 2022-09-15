6daba20fcba7e0f2d54ae88fcb03083332a30b9e
kawamou <kwmkzsd@gmail.com>
2022-09-15 12:39:40 +0900

    Routerを追加

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+const port string = ":8080"
+
+// Router
+type Router struct {
+	Engine *gin.Engine
+}
+
+// NewRouter ...
+func NewRouter() *Router {
+	engine := gin.New()
+	return &Router{
+		Engine: engine,
+	}
+}
+
+// Run ...
+func (r *Router) Run(port string) error {
+	ctx := context.Background()
+	c := NewClient(ctx)
+	pingHandler := NewPingHandler()
+	nyuwaizeHandler := NewNyuwaizeHandler(c)
+	api := r.Engine.Group("")
+	api.GET("/ping", pingHandler.Handle)
+	// api.POST("/analyze-syntax/:text")
+	api.GET("/nyuwaize/:text", nyuwaizeHandler.Handle)
+	return r.Engine.Run(port)
+}
+
+func main() {
+	log.Println(NewRouter().Run(port))
+}
